7ba50dc9e96d60a974c68f3cbe786e2fc2f6023a
pwilson77 <p.o.yirenkyi@gmail.com>
2019-08-25 01:25:19 +0000

    if added the app can log weather data in the console

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+var WeatherAppModule = (function () {
+    const toggleShowAndHide = () => {
+
+    };
+    const getCityWeather = (city) => {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=99a238e2a89a4068350516ce6a439bec`, { mode: 'cors' })
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (response) {
+                console.log(response)
+            });
+    };
+    const getFormData = () => {
+        const temp = document.getElementById('city').value;
+        temp.trim(); // remove whitespace
+        const city = temp.charAt(0).toUpperCase() + temp.slice(1); //Capitalise firstletter
+        getCityWeather(city);
+    };
+
+    return {
+        getFormData
+    };
+})();
