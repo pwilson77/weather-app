b62e8056bcd1d41e92d031eede7e871bdc06db31
pwilson77 <p.o.yirenkyi@gmail.com>
2019-08-27 17:28:50 +0000

    fixed issues raised in review

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,76 @@
-'use strict';
+"use strict";
 
-var weatherAppModule = (function () {
-    const displayWeatherData = (data) => {
-        const cityName = document.getElementById('citytitle');
-        const content1 = document.getElementById('content1');
-        const content2 = document.getElementById('content2');
+var weatherAppModule = (function() {
+  const displayWeatherData = (data, unit) => {
+    const cityName = document.getElementById("citytitle");
+    const content1 = document.getElementById("content1");
+    const content2 = document.getElementById("content2");
+    const card = document.getElementsByClassName("card")[0];
+    let temperatureUnit;
 
-        const text = ` The temperature is ${Math.floor(data.main.temp)} Kelvin , a humidity of ${Math.floor(data.main.humidity)} % 
-        and a pressure of ${Math.floor(data.main.pressure)} hPa`;
+    unit === "metric"
+      ? (temperatureUnit = "Celcius")
+      : (temperatureUnit = "Fahrenheit");
 
-        const text2 = document.createElement('p');
+    const { name, main, weather } = data;
 
-        cityName.innerText = data.name;
-        content1.innerText = text;
-        text2.innerText = data.weather[0].description;
-        content2.innerText = '';
+    const text = `
+     The temperature is ${Math.floor(main.temp)} 
+     ${temperatureUnit} , a humidity of ${Math.floor(main.humidity)} % 
+     and a pressure of ${Math.floor(main.pressure)} hPa`;
 
-        const img = document.createElement('img');
-        img.setAttribute('src', `http://openweathermap.org/img/w/${data.weather[0].icon}.png`);
+    const text2 = document.createElement("p");
 
-        content2.append(text2, img);
+    cityName.innerText = name;
+    content1.innerText = text;
+    text2.innerText = weather[0].description;
+    content2.innerText = "";
 
-    };
-    const getCityWeather = (city) => {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=99a238e2a89a4068350516ce6a439bec`, { mode: 'cors' })
-            .then(function (response) {
-                return response.json()
-            })
-            .then(function (response) {
-                displayWeatherData(response);
-                console.log(response);
-            }).catch(alert('Enter a valid city name'));
-    };
-    const getFormData = () => {
-        const temp = document.getElementById('city').value;
-        temp.trim(); // remove whitespace
-        const city = temp.charAt(0).toUpperCase() + temp.slice(1); //Capitalise firstletter
-        getCityWeather(city);
-    };
+    const img = document.createElement("img");
+    img.setAttribute(
+      "src",
+      `http://openweathermap.org/img/w/${weather[0].icon}.png`
+    );
 
-    return {
-        getFormData
-    };
+    content2.append(text2, img);
+
+    if (card.style.display === "") {
+      card.style.display = "block";
+    }
+  };
+  const handleReset = () => {
+    const errorCon = document.getElementById("error-con");
+    const card = document.getElementsByClassName("card")[0];
+
+    card.style.display = "";
+    errorCon.style.display = "";
+  };
+  const handleError = () => {
+    const errorCon = document.getElementById("error-con");
+    if (errorCon.style.display === "") {
+      errorCon.style.display = "block";
+    }
+  };
+  async function getCityWeather(city, unit) {
+    const fetchUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=99a238e2a89a4068350516ce6a439bec`;
+    try {
+      const data = await fetch(fetchUrl, { mode: "cors" });
+      const response = await data.json();
+      displayWeatherData(response, unit);
+    } catch {
+      handleError();
+    }
+  }
+  const getFormData = () => {
+    handleReset();
+    const temp = document.getElementById("city").value;
+    temp.trim();
+    const city = temp.charAt(0).toUpperCase() + temp.slice(1);
+    const unit = document.getElementById("temp").value;
+    getCityWeather(city, unit);
+  };
+
+  return {
+    getFormData
+  };
 })();
